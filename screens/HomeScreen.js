3fc2f6e1050796715261a3bc6599c51c0d367444
Mark <mgarnick@techtonicgroup.com>
2020-02-04 13:06:20 -0700

    added react-native-localization and i18n-js and can switch languages from settings

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,46 @@
 import React, {PureComponent} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, I18nManager} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18n-js';
 
+const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  ar: () => require('../translations/arabic.json'),
+  en: () => require('../translations/english.json'),
+  fr: () => require('../translations/french.json'),
+};
+
+const translate = (key, config) => i18n.t(key, config);
+const setI18Config = () => {
+  const fallback = {languageTag: 'en', isRTL: false};
+  const {languageTag, isRTL} =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
+
+  I18nManager.forceRTL(isRTL);
+
+  i18n.translations = {[languageTag]: translationGetters[languageTag]()};
+  i18n.locale = languageTag;
+};
 class HomeScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+    setI18Config();
+  }
+
+  componentDidMount() {
+    RNLocalize.addEventListener('change', this.handleLocalizationChange);
+  }
+
+  componentWillUnmount() {
+    RNLocalize.removeEventListener('change', this.handleLocalizationChange);
+  }
+
+  handleLocalizationChange = () => {
+    setI18Config();
+    this.forceUpdate();
+  };
   navDetails = () => {
     const {navigation} = this.props;
     navigation.navigate('Details');
@@ -12,6 +50,7 @@ class HomeScreen extends PureComponent {
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text onPress={this.navDetails}>Home Screen</Text>
         <Icon name="home" size={25} color="blue" />
+        <Text>{translate('hello')}</Text>
       </View>
     );
   }
