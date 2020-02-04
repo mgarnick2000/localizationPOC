f8d413497280fa0c72f978dd887eab9bcb65f87d
Mark <mgarnick@techtonicgroup.com>
2020-02-04 10:52:07 -0700

    add vector icons to android

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 class HomeScreen extends PureComponent {
   navDetails = () => {
@@ -10,6 +11,7 @@ class HomeScreen extends PureComponent {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text onPress={this.navDetails}>Home Screen</Text>
+        <Icon name="home" size={25} color="blue" />
       </View>
     );
   }
